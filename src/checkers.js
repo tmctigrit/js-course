f6f79eeda17e8dedfab324f4d75232f113371fbe
tmctigrit <mctigrit.tricia@gmail.com>
2015-01-02 21:29:23 -0600

    First Commit.

--- src/checkers.js
+++ src/checkers.js
@@ -22,3 +22,27 @@ var selectSquare = function(row, col) {
     $(document).trigger('invalidMove', "You selected an empty spot.");
   }
 };
+
+
+//This will take in a pair of coordinates.  The first pair represents
+//the starting position of a piece you want to move.  The second represents
+//the position that you want it to move to.  The function will check if the
+//move is valid.  If it is, it will execute. Otherwise, it will error out.
+var attemptMove = function(row1, col1, row2, col2) {
+
+};
+
+
+
+//This will execute the movement of the piece.  Helper function called by attemptMove.
+var makeMove = function(row1, col1, row2, col2) {
+
+};
+
+
+//This function will remove the piece at the given coordinate. Helper function called
+//by attemptMove.
+var removePiece = function(row,col) {
+
+};
+
